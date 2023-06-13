ae9115247001f04e1c74ea4cc2cdb54217e164b6
TimvdVooren <timvandervooren@gmail.com>
2023-06-13 14:42:10 +0200

    Initialized project

---
+++ AbnTest/AbnTest/AbnTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbnTestApp.swift
+//  AbnTest
+//
+//  Created by Tim van der Vooren on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct AbnTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
